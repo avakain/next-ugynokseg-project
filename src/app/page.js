862ad66ec35e599cbb72c6e74b99f9064599c6d8
avakain <steveart26@gmail.com>
2023-06-11 09:21:02 +0200

    Added data fetching to mainpage component and connect them whit firebase
    Added design element to page components

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,22 @@
 "use client"
 import Introducion from "./components/intro/Introducion"
-import Results from "./components/results/Results"
-import Carousel from "./components/carousel/Carousel"
+import Results from "./components/resulticonblock/Results"
+import Carousel from "./components/testimonials/Testimonials"
 import Hero from "./components/hero/Hero";
 import Influencer from "./components/influencers/Influencers"
 import Campaign from "./components/campaign/Campaign";
 import WeBelive from "./components/webelive/WeBelive";
 
-
-
 export default function Home() {
 
   return (
-
     <main>
       <Hero heading='Kimagasló elérés márkádnak Influencerekkel' message='Kreatív stratégiáink és hatékony kampányaink segítségével garantáltan felhívjuk rád a figyelmet és növeljük márkád ismertségét. Lépj velünk a következő szintre!' />
       <div className="conent">
         <Introducion
           header={'Növeld az online elérésed'}
-          title={'Segítünk  TikTok és YouTube short  videók készítésében, kezeljük a céged Instagram és Facebook oldalát. '}
-          title2={'Kampányaink segítségével garantáltan felhívjuk rád a figyelmet és növeljük márkád ismertségét. Lépj velünk a következő szintre!'}
+          title={'Segítünk  TikTok és YouTube short  videók készítésében, kezeljük a céged Instagram és Facebook oldalad. '}
+          title2={'Függetlenül attól, hogy milye céljaid vannak, mekkora a vállalkozásod, vagy hol helyezkedsz el, Mi segítségünkkel elérheted a célközönségedet. Légy te a következő siker strorynk!'}
         />
         <Results />
         <Carousel />
