f129b2e2c8e5ab5a5ac9b21d7aa560651eb31d6f
avakain <114566751+avakain@users.noreply.github.com>
2023-07-01 08:59:14 +0200

    Fix html sorce rendering problem
    and added auth checker only the admin page
    
    fix form problem
    
    restructure admin page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,35 +1,18 @@
-'use client'
+"use client"
 import './globals.css'
 import { Raleway } from 'next/font/google'
-import NavBar from './components/navbar/Navbar'
-import Footer from './components/footer/footer'
-import { AuthContextProvider } from '@/app/context/AuthContext'
-import { usePathname } from 'next/navigation'
-import { Provider } from '@/app/context/AdminContext'
 
 const raleway = Raleway({
   subsets: ['latin'],
   weight: ['400', '700']
 })
 
-
 export default function RootLayout({ children }) {
-  const pathname = usePathname();
-
-  const shouldHideNavAndFooter = pathname === '/login-admin' ||
-    pathname === '/admin' ||
-    pathname === '/success';
 
   return (
     <html lang="en">
       <body className={raleway.className}>
-        <AuthContextProvider>
-          <Provider>
-            {!shouldHideNavAndFooter && <NavBar />}
-            {children}
-            {!shouldHideNavAndFooter && <Footer />}
-          </Provider>
-        </AuthContextProvider>
+        {children}
       </body>
     </html>
   )
