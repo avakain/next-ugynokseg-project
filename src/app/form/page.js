d9ec7677cb3cc4921d7557fed1fa5271282f26ff
avakain <114566751+avakain@users.noreply.github.com>
2023-07-01 19:54:38 +0200

    added metadata to form and gdpr page

--- src/app/form/page.js
+++ src/app/form/page.js
@@ -7,6 +7,31 @@ const Form = dynamic(
   { ssr: false }
 )
 
+export const metadata = {
+  title: 'Az Ügynökség - Influencer Marketing a Sikerért',
+  description: 'Az influencer marketing egy nagyon hatékony eszköz a vállalkozások számára, mert lehetővé teszi, hogy közvetlen kapcsolatot építsenek ki a célközönséggel.',
+  icons: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      url: '/favicon/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      url: '/favicon/favicon-16x16.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      url: '/favicon/apple-touch-icon.png',
+    },
+  ],
+}
+
+
 export default function page() {
   return (
     <div>
--- src/app/gdpr/page.js
+++ src/app/gdpr/page.js
@@ -2,6 +2,30 @@ import GDPRComponent from "./GDPRComponent"
 import Link from "next/link"
 import Button from "../components/button/Button"
 
+export const metadata = {
+  title: 'Az Ügynökség - Influencer Marketing a Sikerért',
+  description: 'Az influencer marketing egy nagyon hatékony eszköz a vállalkozások számára, mert lehetővé teszi, hogy közvetlen kapcsolatot építsenek ki a célközönséggel.',
+  icons: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      url: '/favicon/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      url: '/favicon/favicon-16x16.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      url: '/favicon/apple-touch-icon.png',
+    },
+  ],
+}
+
 export default function page() {
   return (
     <div className='sm:m-auto md:max-w-xl'>
